08eda1099671c6f8767703e185acfdaeb869b73f
Tamuna Janezashvili <95763995+Tamouna1@users.noreply.github.com>
2022-05-09 17:15:40 +0400

    counter

--- javascript/counter.js
+++ javascript/counter.js
@@ -1,11 +1,17 @@
-
 let countEl = document.getElementById("count-el")
-
+let saveEl = document.getElementById("save-el")
 console.log(countEl)
 
 let count = 0
 
 document.getElementById("increment-btn").onclick = function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
+}
+
+document.getElementById("save-btn").onclick = function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr
+    console.log(count)
+    countEl.textContent = 0;
 }
